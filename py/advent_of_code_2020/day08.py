7ffc6ea7f8aa6adac0dc745c2d4132a1f6642b6e
Andrew Lim <andrew.b.lim@gmail.com>
2020-12-08 09:13:10 -0500

    Add day08 (py)

---
+++ py/advent_of_code_2020/day08.py
@@ -0,0 +1,55 @@
+import sys
+
+
+def parse_instruction(line):
+    op, arg = line.split(" ")
+    return op, int(arg)  # works with + sign!
+
+
+def execute_instructions(instructions, start_pos=0, start_acc=0):
+    pos = start_pos
+    acc = start_acc
+    first_visit = {}
+    step = 0
+    while pos not in first_visit and pos < len(instructions):
+        step += 1
+        first_visit[pos] = step
+        op, arg = instructions[pos]
+        if op == "acc":
+            acc += arg
+            pos += 1
+        elif op == "jmp":
+            pos += arg
+        elif op == "nop":
+            pos += 1
+        else:
+            raise RuntimeError(f"unrecognized op {op}")
+    if pos > len(instructions):
+        raise RuntimeError(f"sent beyond one past the last instruction: {pos}")
+    elif pos == len(instructions):
+        return first_visit, acc, True
+    else:
+        return first_visit, acc, False
+
+
+def fix_jmp_instruction(instructions):
+    for i, (op, arg) in enumerate(instructions):
+        if op == "jmp":
+            new_instructions = list(instructions)
+            new_instructions[i] = ("nop", arg)
+            _, acc, success = execute_instructions(new_instructions)
+            if success:
+                return i, acc
+    raise RuntimeError("could not fix program")
+
+
+if __name__ == "__main__":
+    with open(sys.argv[1], "r") as fp:
+        instructions = [parse_instruction(line.strip())
+                        for line in fp.readlines()]
+    print("Part 1:")
+    _, acc, _ = execute_instructions(instructions)
+    print(acc)
+    print("Part 2:")
+    _, acc = fix_jmp_instruction(instructions)
+    print(acc)
---
+++ py/tests/test_day08.py
@@ -0,0 +1,24 @@
+from .context import advent_of_code_2020
+from advent_of_code_2020.day08 import *
+
+
+test_text = """
+nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6
+""".strip()
+
+instructions = [parse_instruction(line.strip()) for line in test_text.splitlines()]
+
+def test_execute_instructions():
+    assert execute_instructions(instructions) == \
+        ({0: 1, 1: 2, 2: 3, 3: 6, 4: 7, 6: 4, 7: 5}, 5, False)
+
+def test_fix_jmp_instruction():
+    assert fix_jmp_instruction(instructions) == (7, 8)
